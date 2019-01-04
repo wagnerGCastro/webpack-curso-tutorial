e2a8d4f9b01d42c9a3aa9f0b50c662827b53b204
wagner <wagner@futmanager.com>
2019-01-04 18:56:53 -0200

    11-atualizando-browser-sem-refresh-com-HMR

--- src/js/app.js
+++ src/js/app.js
@@ -6,5 +6,10 @@
 	//import { sayHelloTo } from './hello.js';
 
 const span = document.createElement('span');
-span.innerText = sayHelloTo('JS Developer');
+span.innerText = sayHelloTo('Javascript Developer');
 document.body.appendChild(span);
+
+if (module.hot) {
+	module.hot.accept();
+	module.hot.dispose( () => span.parentNode.removeChild(span) );
+}
