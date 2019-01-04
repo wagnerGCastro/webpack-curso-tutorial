e0cc2d6046b0c950fbd4ce5814c8908171c474de
wagner <wagner@futmanager.com>
2019-01-04 17:22:53 -0200

    05-criando-bundle-via-limha-de-comando

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,6 @@
 // 2 - maniera de importar arquivo
 	//import { sayHelloTo } from './hello.js';
 
-const span = docuemnt.createElement('span');
+const span = document.createElement('span');
 span.innerText = sayHelloTo('Javascript Developer');
-docuemnt.body.appendChild(span);
+document.body.appendChild(span);
